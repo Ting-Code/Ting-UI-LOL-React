979fa4714aef3c81616b984d0ff640dc7c0f820f
TINGCYGF <1127163160@qq.com>
2021-05-17 16:17:54 +0800

    完成Button组件测试

---
+++ src/lib/components/Button/button.test.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import Button, { ButtonProps } from './Button'
+const defaultProps = {
+  onClick: jest.fn()
+}
+
+const testProps: ButtonProps = {
+  types: 'primary',
+  size: 'lg',
+  className: 'klass'
+}
+const disabledProps: ButtonProps = {
+  disabled: true,
+  onClick: jest.fn(),
+}
+describe('test Button component', () => {
+  it('should render the correct default button', () => {
+    const wrapper = render(<Button {...defaultProps}>Nice</Button>)
+    const element = wrapper.getByText('Nice') as HTMLButtonElement
+    expect(element).toBeInTheDocument()
+    expect(element.tagName).toEqual('BUTTON')
+    expect(element).toHaveClass('ting-button ting-button-default')
+    expect(element.disabled).toBeFalsy()
+    fireEvent.click(element)
+    expect(defaultProps.onClick).toHaveBeenCalled()
+  })
+  it('should render the correct component based on different props', () => {
+    const wrapper = render(<Button {...testProps}>Nice</Button>)
+    const element = wrapper.getByText('Nice')
+    expect(element).toBeInTheDocument()
+    expect(element).toHaveClass('ting-button-primary ting-button-lg klass')
+  })
+  it('should render a link when btnType equals link and href is provided', () => {
+    const wrapper = render(<Button types='link' href="http://dummyurl">Link</Button>)
+    const element = wrapper.getByText('Link')
+    expect(element).toBeInTheDocument()
+    expect(element.tagName).toEqual('A')
+    expect(element).toHaveClass('ting-button ting-button-link')
+  })
+  it('should render disabled button when disabled set to true', () => {
+    const wrapper = render(<Button {...disabledProps}>Nice</Button>)
+    const element = wrapper.getByText('Nice') as HTMLButtonElement
+    expect(element).toBeInTheDocument()
+    expect(element.disabled).toBeTruthy()
+    fireEvent.click(element)
+    expect(disabledProps.onClick).not.toHaveBeenCalled()
+  })
+})
