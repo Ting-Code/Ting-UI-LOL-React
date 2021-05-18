970d3c20dc3d19a67b5a6b76661efd0498eb39ad
TINGCYGF <1127163160@qq.com>
2021-05-18 17:41:25 +0800

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import {Button} from './lib/index';
+import React, {useState} from 'react';
 import './lib/style/index.scss';
+import { Home } from "./view/Home/Home";
+import {HashRouter, Route, Switch} from 'react-router-dom';
+import {Doc} from "./view/Doc/Doc";
+import {Topnav} from "./view/components/Topnav/Topnav";
+const AsideContext = React.createContext<{asideVisible: boolean, setAsideVisible:any, aside: boolean, setAside:any}>({asideVisible: false, setAsideVisible: {}, aside: false, setAside:{}});
 
 function App() {
+  const width = document.documentElement.clientWidth
+  const [asideVisible, setAsideVisible] = useState(width > 500)
+  const [aside, setAside] = useState(width > 500)
   return (
     <div className="App">
-      <Button disabled>
-        按钮
-      </Button>
+      <AsideContext.Provider value={{asideVisible, setAsideVisible, aside, setAside}}>
+        <Topnav/>
+        <HashRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact={false} path="/doc" component={Doc} />
+          </Switch>
+        </HashRouter>
+      </AsideContext.Provider>
     </div>
   );
 }
 
 export default App;
+export {AsideContext}
