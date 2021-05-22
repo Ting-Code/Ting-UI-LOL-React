90b66440f15ef7cf6045b9005bf59a9cd965e502
TINGCYGF <1127163160@qq.com>
2021-05-22 17:16:21 +0800

    完成spread组件

--- src/lib/components/Transition/Spread.tsx
+++ src/lib/components/Transition/Spread.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useState} from "react";
 import { CSSTransition } from "react-transition-group"
 import {CSSTransitionProps} from "react-transition-group/CSSTransition";
 
@@ -21,97 +21,51 @@ const Spread: React.FC<TransitionProps & CSSTransitionProps> = (props) => {
     ...restProps
   } = props
 
-  const [horizontalTransition, setHorizontalTransition] = useState(`300ms width ease-in-out, 300ms padding-left ease-in-out, 300ms padding-right ease-in-out`)
-  const [verticalTransition, setVerticalTransition] = useState(`300ms height ease-in-out, 300ms padding-top ease-in-out, 300ms padding-bottom ease-in-out`)
+  const [horizontalTransition, setHorizontalTransition] = useState(`300ms width ease-in-out`)
+  const [verticalTransition, setVerticalTransition] = useState(`300ms height ease-in-out`)
   const [originTransition, setOriginTransition] = useState(`300ms all ease-in-out`)
-  const spread = useRef<HTMLDivElement>(null)
+
 
   useEffect(() => {
-    setHorizontalTransition(`${+timeout}ms width ease-in-out, ${+timeout}ms margin-left ease-in-out, ${+timeout}ms margin-right ease-in-out`)
-    setVerticalTransition(`${+timeout}ms height ease-in-out, ${+timeout}ms margin-top ease-in-out, ${+timeout}ms margin-bottom ease-in-out`)
+    setHorizontalTransition(`${+timeout}ms width ease-in-out`)
+    setVerticalTransition(`${+timeout}ms height ease-in-out`)
     setOriginTransition(`${+timeout}ms all ease-in-out`)
   }, [timeout])
 
-  const [oldPaddingLeft, setOldPaddingLeft] = useState<string>('')
-  const [oldPaddingRight, setOldPaddingRight] = useState<string>('')
-  const [oldPaddingTop, setOldPaddingTop] = useState<string>('')
-  const [oldPaddingBottom, setOldPaddingBottom] = useState<string>('')
+
   const [oldOverflow, setOldOverflow] = useState<string>('')
 
 
   const beforeEnter = (el: HTMLElement) => {
     if (animation === "horizontal") {
       el.style.transition = horizontalTransition;
-      setOldPaddingLeft(el.style.paddingLeft)
-      setOldPaddingRight(el.style.paddingRight)
       el.style.width = '0';
-      el.style.paddingLeft = '0';
-      el.style.paddingRight = '0';
     } else if(animation === "vertical"){
       el.style.transition = verticalTransition;
-      setOldPaddingTop(el.style.paddingTop)
-      setOldPaddingBottom(el.style.paddingBottom)
       el.style.height = '0';
-      el.style.paddingTop = '0';
-      el.style.paddingBottom = '0';
     }else {
       el.style.transition = originTransition;
-      setOldPaddingTop(el.style.paddingTop)
-      setOldPaddingBottom(el.style.paddingBottom)
-      setOldPaddingLeft(el.style.paddingLeft)
-      setOldPaddingRight(el.style.paddingRight)
-      console.log(el.style.paddingTop, el.style.paddingBottom, el.style.paddingLeft, el.style.paddingRight)
-
       el.style.height = '0';
-      el.style.paddingTop = '0';
-      el.style.paddingBottom = '0';
       el.style.width = '0';
-      el.style.paddingLeft = '0';
-      el.style.paddingRight = '0';
+
     }
   }
   const enter = (el:HTMLElement) => {
     setOldOverflow(el.style.overflow)
-    if (animation === "horizontal") {
-      if (el.scrollWidth !== 0) {
-        el.style.width = el.scrollWidth + 'px';
-      } else {
-        el.style.width = '';
-      }
-      el.style.paddingLeft = oldPaddingLeft;
-      el.style.paddingRight = oldPaddingRight;
-
-    } else if(animation === "vertical"){
-      if (el.scrollHeight !== 0) {
-        el.style.height = el.scrollHeight + 'px';
-      } else {
-        el.style.height = '';
-      }
-      el.style.paddingTop = oldPaddingTop;
-      el.style.paddingBottom = oldPaddingBottom;
+    if (animation === "horizontal" && el.scrollWidth !== 0) {
+      el.style.width = el.scrollWidth + 'px';
+    } else if(animation === "vertical" && el.scrollHeight !== 0){
+      el.style.height = el.scrollHeight + 'px';
     }else {
-      if (el.scrollHeight !== 0) {
-        el.style.height = el.scrollHeight + 'px';
-      } else {
-        el.style.height = '';
-      }
-      if (el.scrollWidth !== 0) {
-        el.style.width = el.scrollWidth + 'px';
-      } else {
-        el.style.width = '';
-      }
-      el.style.paddingTop = oldPaddingTop;
-      el.style.paddingBottom = oldPaddingBottom;
-      el.style.paddingLeft = oldPaddingLeft;
-      el.style.paddingRight = oldPaddingRight;
-      console.log(oldPaddingTop, oldPaddingBottom, oldPaddingLeft, oldPaddingRight)
+      el.style.height = el.scrollHeight + 'px';
+      el.style.width = el.scrollWidth + 'px';
     }
     el.style.overflow = 'hidden';
   }
   const afterEnter = (el:HTMLElement) => {
     el.style.transition = '';
     el.style.overflow = oldOverflow;
-    if(  animation === "horizontal"){
+    if( animation === "horizontal"){
       el.style.width = ''
     }else if(animation === 'vertical'){
       el.style.height = ''
@@ -123,19 +77,11 @@ const Spread: React.FC<TransitionProps & CSSTransitionProps> = (props) => {
   const beforeLeave = (el:HTMLElement) => {
     setOldOverflow(el.style.overflow)
     if (animation === "horizontal") {
-      setOldPaddingLeft(el.style.paddingLeft)
-      setOldPaddingRight(el.style.paddingRight)
       el.style.width = el.scrollWidth + 'px';
     } else if(animation === "vertical"){
-      setOldPaddingTop(el.style.paddingTop)
-      setOldPaddingBottom(el.style.paddingBottom)
       el.style.height = el.scrollHeight + 'px';
     }else {
-      setOldPaddingLeft(el.style.paddingLeft)
-      setOldPaddingRight(el.style.paddingRight)
       el.style.width = el.scrollWidth + 'px';
-      setOldPaddingTop(el.style.paddingTop)
-      setOldPaddingBottom(el.style.paddingBottom)
       el.style.height = el.scrollHeight + 'px';
     }
     el.style.overflow = 'hidden';
@@ -145,48 +91,18 @@ const Spread: React.FC<TransitionProps & CSSTransitionProps> = (props) => {
       if (el.scrollWidth !== 0) {
         el.style.transition = horizontalTransition;
         el.style.width = '0';
-        el.style.paddingLeft = '0';
-        el.style.paddingRight = '0';
       }
     } else if(animation === 'vertical'){
       if (el.scrollHeight !== 0) {
         el.style.transition = verticalTransition;
         el.style.height = '0';
-        el.style.paddingTop = '0';
-        el.style.paddingBottom = '0';
       }
     }else {
       if (el.scrollHeight !== 0 || el.scrollWidth !== 0) {
         el.style.transition = originTransition;
         el.style.height = '0';
-        el.style.paddingTop = '0';
-        el.style.paddingBottom = '0';
         el.style.width = '0';
-        el.style.paddingLeft = '0';
-        el.style.paddingRight = '0';
       }
-    }
-  }
-  const afterLeave = (el:HTMLElement) => {
-    el.style.transition = '';
-    el.style.overflow = oldOverflow;
-    if (animation === "horizontal") {
-      el.style.width = '';
-      el.style.paddingLeft = oldPaddingLeft;
-      el.style.paddingRight = oldPaddingRight;
-    } else if(animation === 'vertical') {
-      el.style.height = '';
-      el.style.paddingTop = oldPaddingTop;
-      el.style.paddingBottom = oldPaddingBottom;
-    } else {
-      el.style.width = '';
-      el.style.paddingLeft = oldPaddingLeft;
-      el.style.paddingRight = oldPaddingRight;
-      el.style.height = '';
-      el.style.paddingTop = oldPaddingTop;
-      el.style.paddingBottom = oldPaddingBottom;
-      console.log(oldPaddingTop, oldPaddingBottom, oldPaddingLeft, oldPaddingRight)
-
     }
   }
 
@@ -198,11 +114,11 @@ const Spread: React.FC<TransitionProps & CSSTransitionProps> = (props) => {
       onEntering={el => enter(el)}
       onEntered={el => afterEnter(el)}
       onExit={el => beforeLeave(el)}
-      onExiting={el => leave(el)}
-      onExited={el => afterLeave(el)}
+      onExiting={el =>leave(el)}
+      onExited={el => afterEnter(el)}
       {...restProps}
     >
-      {wrapper ? <div ref={spread}>{children}</div> : children}
+      {wrapper ? <div>{children}</div> : children}
     </CSSTransition>
   )
 }
