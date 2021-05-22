9e8efeb83bb48aa8ada094ad99ce80e8223467df
TINGCYGF <1127163160@qq.com>
2021-05-22 11:20:56 +0800

    界面调整

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,3 +1,7 @@
 
-
 export { default as Button } from './components/Button/Button'
+export { default as Icon } from './components/Icon/Icon'
+
+export { Menu } from './components/Menu/Menu'
+export { MenuItem } from './components/Menu/MenuItem'
+
--- src/view/components/Main/Main.tsx
+++ src/view/components/Main/Main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import {Route, Switch } from "react-router-dom";
-import {ButtonDoc} from "../LibDoc/ButtonDoc";
+import {ButtonDoc} from "../LibDoc/BurronDoc/ButtonDoc";
+
+import {MenuDoc} from "../LibDoc/MenuDoc/MenuDoc";
+
 import './style.scss'
 
 const Main = () => {
@@ -8,6 +11,7 @@ const Main = () => {
     <div className="main-doc">
       <Switch>
         <Route path="/doc/button" component={ButtonDoc} />
+        <Route path="/doc/menu" component={MenuDoc} />
       </Switch>
     </div>
   )
