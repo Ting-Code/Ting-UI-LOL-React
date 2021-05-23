72b1756424e82f0edc7c60bd1717f83e29970e58
TINGCYGF <1127163160@qq.com>
2021-05-23 17:59:48 +0800

    MenuDoc

---
+++ src/view/components/LibDoc/MenuDoc/MenuDoc.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import {MenuBase} from "./MenuBase";
+
+const MenuDoc: React.FC = () => {
+
+  return (
+    <div>
+      <MenuBase/>
+    </div>
+  )
+}
+
+export {MenuDoc}
