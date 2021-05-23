02fb9f30a92123bb04aa376e47a419bb7470889e
TINGCYGF <1127163160@qq.com>
2021-05-23 18:00:01 +0800

    MenuDoc基础使用界面

---
+++ src/view/components/LibDoc/MenuDoc/MenuBase.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {Menu, MenuItem} from "../../../../lib";
+import {CodeBox} from "../../CodeBox/CodeBox";
+import {SubMenu} from "../../../../lib/components/Menu/SubMenu";
+
+
+const MenuBase:React.FC = () => {
+  const code = `
+`
+
+  return (
+    <CodeBox
+      title="基础使用"
+      description="描述"
+      defaultVisible={true}
+      code={code}>
+      <Menu defaultIndex={'2'} mode="horizontal">
+        <MenuItem>
+          第一项
+        </MenuItem>
+        <SubMenu title="下拉菜单">
+          <MenuItem>
+            下拉一
+          </MenuItem>
+          <MenuItem>
+            下拉一
+          </MenuItem>
+          <MenuItem>
+            下拉一
+          </MenuItem>
+          <MenuItem>
+            下拉一
+          </MenuItem>
+        </SubMenu>
+        <MenuItem>
+          第三项
+        </MenuItem>
+      </Menu>
+    </CodeBox>
+  )
+}
+
+
+export {MenuBase};
