3b4514b7f0209cb335956496c45f24c02393a914
TINGCYGF <1127163160@qq.com>
2021-05-26 14:05:50 +0800

    Input组件

---
+++ src/lib/components/Input/Input.tsx
@@ -0,0 +1,62 @@
+import React, { FC, ReactElement, InputHTMLAttributes, ChangeEvent } from 'react'
+import classNames from 'classnames'
+import Icon from "../Icon/Icon";
+
+type InputSize = 'lg' | 'sm'
+export interface InputProps extends Omit<InputHTMLAttributes<HTMLElement>, 'size' > {
+  /**是否禁用 Input */
+  disabled?: boolean;
+  /**设置 input 大小，支持 lg 或者是 sm */
+  size?: InputSize;
+  /**添加图标，在右侧悬浮添加一个图标，用于提示 */
+  icon?: string;
+  /**添加前缀 用于配置一些固定组合 */
+  prepend?: string | ReactElement;
+  /**添加后缀 用于配置一些固定组合 */
+  append?: string | ReactElement;
+  onChange? : (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+
+export const Input: FC<InputProps> = (props) => {
+  const {
+    disabled,
+    size,
+    icon,
+    prepend,
+    append,
+    style,
+    ...restProps
+  } = props
+  const inputClasses = classNames('ting-input-wrapper', {
+    [`input-size-${size}`]: size,
+    'is-disabled': disabled,
+    'input-group': prepend || append,
+    'input-group-append': !!append,
+    'input-group-prepend': !!prepend
+  })
+  const fixControlledValue = (value: any) => {
+    if (typeof value === 'undefined' || value === null) {
+      return ''
+    }
+    return value
+  }
+  if('value' in props) {
+    delete restProps.defaultValue
+    restProps.value = fixControlledValue(props.value)
+  }
+  return (
+    <div className={inputClasses} style={style}>
+      {prepend && <div className="ting-input-group-prepend">{prepend}</div>}
+      {icon && <div className="icon-wrapper"><Icon name={icon}/></div>}
+      <input
+        className="ting-input-inner"
+        disabled={disabled}
+        {...restProps}
+      />
+      {append && <div className="ting-input-group-append">{append}</div>}
+    </div>
+  )
+}
+
+export default Input;
