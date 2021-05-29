49259fe4b31bc1dee3e1282c6a2ff1408e40477e
TINGCYGF <1127163160@qq.com>
2021-05-29 15:26:39 +0800

    完成alert组件调用dialog

---
+++ src/lib/components/Dialog/alert.tsx
@@ -0,0 +1,28 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import {Dialog} from "./Dialog";
+
+interface content {
+  children: JSX.Element
+  title: string
+}
+
+const alert = (content: content) => {
+  const component = <Dialog visible={true}
+                            title={content.title}
+                            onClose={() => {
+    ReactDOM.render(React.cloneElement(component, {
+      visible: false
+    }), div)
+    ReactDOM.unmountComponentAtNode(div)
+    div.remove()
+  }}>
+    {content.children}
+  </Dialog>
+  const div = document.createElement('div')
+  document.body.append(div)
+  ReactDOM.render(component, div)
+}
+
+
+export {alert}
