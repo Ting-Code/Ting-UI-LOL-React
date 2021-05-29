6ab860aec8740acb4d8be423097c973cc23071d6
TINGCYGF <1127163160@qq.com>
2021-05-29 15:26:54 +0800

    完成confirm组件调用dialog

---
+++ src/lib/components/Dialog/confirm.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Dialog} from "./Dialog";
+import {Button} from "../Button/Button";
+
+interface content {
+  children: JSX.Element
+  title: string
+}
+
+const confirm = (content: content, yes?: () => void, no?: () => void) => {
+  const onYes = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div)
+    ReactDOM.unmountComponentAtNode(div)
+    div.remove()
+    yes && yes()
+  }
+  const onNo = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div)
+    ReactDOM.unmountComponentAtNode(div)
+    div.remove()
+    no && no()
+  }
+  const component = (
+    <Dialog
+      title={content.title}
+      visible={true} onClose={() => { onNo()}}
+      buttons={[
+        <Button size='sm' onClick={onYes}>确定</Button>,
+        <Button size='sm' onClick={onNo}> 取消</Button>
+      ]}
+    >
+      {content.children}
+    </Dialog>)
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  ReactDOM.render(component, div)
+}
+
+export {confirm}
