e1c20059731d056b1f155fd0d489718386058209
TINGCYGF <1127163160@qq.com>
2021-05-31 16:54:16 +0800

    完成popover组件

--- src/lib/components/Popover/Popover.tsx
+++ src/lib/components/Popover/Popover.tsx
@@ -7,14 +7,14 @@ interface PopoverProps {
   content?: React.ReactNode
 }
 
-
 const Popover: React.FC<PopoverProps> = (props) => {
   const {position, trigger, content, children} = props;
   const [visible, setVisible] = useState(false)
+  const visibleRef = useRef(visible);
+
   const contentWrapper = useRef<HTMLDivElement>(null)
   const triggerWrapper = useRef<HTMLDivElement>(null)
   const popover = useRef<HTMLDivElement>(null)
-
 
   const contentWrapperClass = classNames('ting-popover-content', 'ting-popover-zindex', {
     [`position-${position}`]: true,
@@ -28,27 +28,27 @@ const Popover: React.FC<PopoverProps> = (props) => {
       popover.current!.addEventListener('mouseleave', close)
     }
     return () => {
-      let div = document.querySelector('.ting-content')
-      if (div) {
-        div.parentNode!.removeChild(div)
-      }
-      document.removeEventListener('click', onClickDocument)
+
     }
+    /*eslint-disable*/
   }, [])
   useEffect(() => {
+    visibleRef.current = visible;
     if(visible){
       setTimeout(() => {
+        //挂载后append到body
         positionContent()
         document.addEventListener('click', onClickDocument)
         window.addEventListener("scroll", close, true);
-      },0)
+      })
     }
+    /*eslint-disable*/
   }, [visible])
-  const onClick = (event: any) => {
-    if (triggerWrapper.current!.contains(event.target)) {
-      if (visible) {
+  const onClick = (e: Event) => {
+    if (triggerWrapper.current && triggerWrapper.current.contains(e.target as Element)) {
+      if(visibleRef.current) {
         close()
-      } else {
+      }else {
         open()
       }
     }
@@ -58,33 +58,35 @@ const Popover: React.FC<PopoverProps> = (props) => {
   }
   const close = () => {
     setVisible(false)
-    window.removeEventListener("scroll", close, true)
     document.removeEventListener('click', onClickDocument)
+    window.removeEventListener("scroll", close, true)
   }
   const positionContent = () => {
-    document.body.appendChild(contentWrapper.current!)
-    const {width, height, top, left} = triggerWrapper.current!.getBoundingClientRect()
-    const {height: height2} = contentWrapper.current!.getBoundingClientRect()
-    const positions = {
-      top: {
-        top: top + window.scrollY,
-        left: left + window.scrollX
-      },
-      bottom: {
-        top: top + height + window.scrollY,
-        left: left + window.scrollX
-      },
-      left: {
-        top: top + window.scrollY + (height - height2) / 2,
-        left: left + window.scrollX
-      },
-      right: {
-        top: top + window.scrollY + (height - height2) / 2,
-        left: left + width + window.scrollX
+    if(contentWrapper.current && triggerWrapper.current){
+      document.body.appendChild(contentWrapper.current)
+      const {width, height, top, left} = triggerWrapper.current.getBoundingClientRect()
+      const {height: height2} = contentWrapper.current.getBoundingClientRect()
+      const positions = {
+        top: {
+          top: top + window.scrollY,
+          left: left + window.scrollX
+        },
+        bottom: {
+          top: top + height + window.scrollY,
+          left: left + window.scrollX
+        },
+        left: {
+          top: top + window.scrollY + (height - height2) / 2,
+          left: left + window.scrollX
+        },
+        right: {
+          top: top + window.scrollY + (height - height2) / 2,
+          left: left + width + window.scrollX
+        }
       }
+      contentWrapper.current.style.top = positions[position].top + 'px'
+      contentWrapper.current.style.left = positions[position].left + 'px'
     }
-    contentWrapper.current!.style.top = positions[position].top + 'px'
-    contentWrapper.current!.style.left = positions[position].left + 'px'
   }
   //关闭popover  的事件
   const onClickDocument = (e: Event) => {
@@ -96,16 +98,34 @@ const Popover: React.FC<PopoverProps> = (props) => {
     }
     close()
   }
+  const changeVisible = () => {
+    if(visible){
+      return (
+        <div ref={contentWrapper} className={contentWrapperClass}>
+          <p>{content}</p>
+        </div>
+      )
+    }else {
+      return (
+        <div style={{display: "none"}} ref={contentWrapper} className={contentWrapperClass}>
+          <p>{content}</p>
+        </div>
+      )
+    }
+  }
   return (
     <div className="ting-popover" ref={popover}>
-      {visible && <div ref={triggerWrapper} className={contentWrapperClass}>
-        <p>{content}</p>
-      </div>}
-      <div ref={contentWrapper} className="ting-popover-trigger">
+      {changeVisible()}
+      <div ref={triggerWrapper} className="ting-popover-trigger">
         {children}
       </div>
     </div>
   )
+}
+
+Popover.defaultProps = {
+  trigger: 'click',
+  position: 'top'
 }
 
 export {Popover}
