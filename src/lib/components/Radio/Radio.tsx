6ccf7e181bac73d464bfa1185f6bf0659553908b
TINGCYGF <1127163160@qq.com>
2021-06-01 11:44:18 +0800

    完成Radio组件

--- src/lib/components/Radio/Radio.tsx
+++ src/lib/components/Radio/Radio.tsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import classNames from "classnames";
+import {Transition} from "../Transition/Transition";
 
 interface RadioProps {
-  value: string | number
+  value: string | number;
+  select?: string | number;
+  onChange?: (value: string | number) => void;
+  disabled?: boolean;
 }
-
 const Radio: React.FC<RadioProps> = (props) => {
+  const { value, select, onChange, disabled } = props
+  const [ checked, setChecked ] = useState(false)
+  const spanClass = classNames("ting-radio-click", {
+    'ting-radio-active': checked
+  })
+  const labelClass = classNames('ting-radio-label', {
+    'disabled': disabled
+  })
+  useEffect(() => {
+    if(select === value){
+      setChecked(true)
+    }else {
+      setChecked(false)
+    }
+  }, [select, value])
+  const labelOnChange: React.ChangeEventHandler = (e) => {
+    onChange?.(value)
+  }
   return (
-    <label className='ting-radio-label'>
+    <label className={labelClass}>
       <span>
-        <span className="ting-radio-click"/>
+        <span className={spanClass}>
+          <Transition in={checked} timeout={300} animation={"zoom-in-open"}>
+            <span className="change-span"/>
+          </Transition>
+        </span>
         <input
           type="radio"
           className="ting-radio-input"
+          checked={checked}
+          onChange={labelOnChange}
+          disabled={disabled}
         />
       </span>
       <span className="ting-radio-children">
@@ -21,4 +50,6 @@ const Radio: React.FC<RadioProps> = (props) => {
   )
 }
 
-export {Radio}
+Radio.displayName = "Radio"
+export {Radio};
+export type { RadioProps };
--- src/lib/components/Radio/RadioGroup.tsx
+++ src/lib/components/Radio/RadioGroup.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {FunctionComponentElement, useRef} from "react";
+import {RadioProps} from "./Radio";
 
+interface RadioGroupProps {
+  select?: string | number;
+  onChange?: (value:string | number) => void
+}
 
+const RadioGroup:React.FC<RadioGroupProps> = (props) => {
+  const {select, onChange} = props
+  const RadioGroup = useRef(null)
+  const renderChildren = () => {
+    return React.Children.map(props.children, (child, i) => {
+      const childElement = child as FunctionComponentElement<RadioProps>
+      if (childElement.type.displayName === 'Radio') {
+        return React.cloneElement(childElement, {onChange: onChange, select: select})
+      } else {
+        console.error("RadioGroup组件的子子组件只能为Radio组件")
+      }
+    });
+  }
 
-const RadioGroup:React.FC = () => {
   return (
-    <div>
-
+    <div ref={RadioGroup} className='ting-radio-group'>
+      {renderChildren()}
     </div>
   )
 }
