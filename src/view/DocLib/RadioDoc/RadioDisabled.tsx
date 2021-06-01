6baa7a09951cacf9eea8243cdd4c809c09acbced
TINGCYGF <1127163160@qq.com>
2021-06-01 11:43:55 +0800

    单项选择的radio选项值

---
+++ src/view/DocLib/RadioDoc/RadioDisabled.tsx
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import {CodeBox} from "../../components/CodeBox/CodeBox";
+import {Radio, RadioGroup} from "../../../lib";
+
+
+
+const RadioDisabled:React.FC = () => {
+  const code = `const [select, setSelect] = useState<number | string>(1)
+  return (
+      <RadioGroup select={select} onChange={(value) => {
+        setSelect(value)
+      }}>
+        <Radio value={1}>第一项</Radio>
+        <Radio value={2}>第二项</Radio>
+        <Radio value={3} disabled>第三项</Radio>
+      </RadioGroup>
+  )
+}`
+  const [select, setSelect] = useState<number | string>(1)
+  return (
+    <CodeBox
+      title="禁用选项"
+      description={<p><code>disabled</code>属性定义禁用选项</p>}
+      defaultVisible={true}
+      code={code}>
+      <RadioGroup select={select} onChange={(value) => {
+        setSelect(value)
+      }}>
+        <Radio value={1}>第一项</Radio>
+        <Radio value={2}>第二项</Radio>
+        <Radio value={3} disabled>第三项</Radio>
+      </RadioGroup>
+
+    </CodeBox>
+  )
+}
+
+
+export {RadioDisabled};
