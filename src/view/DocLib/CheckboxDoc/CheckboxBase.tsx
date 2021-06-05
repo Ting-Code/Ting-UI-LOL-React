2dfac5333bb71ad2c2832ce53c8e8506ef83763c
TINGCYGF <1127163160@qq.com>
2021-06-05 09:57:58 +0800

    完成checkbox组件

--- src/view/DocLib/CheckboxDoc/CheckboxBase.tsx
+++ src/view/DocLib/CheckboxDoc/CheckboxBase.tsx
@@ -1,27 +1,38 @@
 import React, {useState} from "react";
 import {CodeBox} from "../../components/CodeBox/CodeBox";
-import {Checkbox} from "../../../lib/components/Checkbox/Checkbox";
+import {Checkbox} from "../../../lib";
 
 
 
 const CheckboxBase:React.FC = () => {
-  const code = ``
+  const code = `const [check, setCheck] = useState(true)
+return (
+    <>
+      <Checkbox value="无极剑圣" checked={check} onChange={(e) => {
+        setCheck(e.target.checked)
+      }}>无极剑圣</Checkbox>
+      <Checkbox value="厄运小姐" checked={false} disabled>厄运小姐</Checkbox>
+    </>
+  )`
 
-  const [check, setCheck] = useState(false)
+  const [check, setCheck] = useState(true)
   return (
     <CodeBox
       title="基础使用"
-      description={<p><code>types</code>属性定义按键样式</p>}
+      description={<p><code>checked</code>属性确定是否选择，<code>disabled</code>属性禁用选框，点击触发<code>onChange</code>事件</p>}
       defaultVisible={true}
       code={code}>
 
-      <Checkbox checked={check} onChange={(e) => {
+      <Checkbox value="无极剑圣" checked={check} indeterminate={false} onChange={(e) => {
         setCheck(e.target.checked)
-      }}>选择</Checkbox>
+      }}>无极剑圣</Checkbox>
+      <Checkbox value="厄运小姐" checked={false} disabled
+      >厄运小姐</Checkbox>
 
     </CodeBox>
   )
 }
+
 
 
 export {CheckboxBase};
