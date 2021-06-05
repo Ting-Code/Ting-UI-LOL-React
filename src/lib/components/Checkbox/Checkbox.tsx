b23437b836babe8b679862a43cd3b21956cc49ca
TINGCYGF <1127163160@qq.com>
2021-06-05 09:58:03 +0800

    完成checkbox组件

--- src/lib/components/Checkbox/Checkbox.tsx
+++ src/lib/components/Checkbox/Checkbox.tsx
@@ -1,22 +1,31 @@
-import React, { useEffect, useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import classNames from "classnames";
 import {Icon} from "../Icon/Icon";
 
 interface CheckboxProps{
-  checked: boolean;
+  checked?: boolean;
   indeterminate?: boolean;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
   disabled?: boolean;
+  value: string;
+  selectOptions?: string[];
+  optionChange?: (selectOptions: string[]) => void
 }
 
 const Checkbox:React.FC<CheckboxProps> = (props) => {
-  const {checked, onChange, indeterminate, children, disabled} = props
+  const {checked, onChange, indeterminate, children, disabled, selectOptions, optionChange, value} = props
+  const [_checked, _setChecked] = useState(checked || false)
   const checkboxRef = useRef<HTMLInputElement | null>(null)
   useEffect(() => {
-    checkboxRef.current!.checked = checked;
+    checked !== undefined && _setChecked(checked)
   }, [checked])
+  useEffect(() => {
+    if(selectOptions){
+      selectOptions?.includes(value) ? _setChecked(true): _setChecked(false)
+    }
+  }, [selectOptions, value])
   const spanClass = classNames("ting-checkbox-click", {
-    'ting-checked-active': checked,
+    'ting-checked-active': _checked,
     'ting-indeterminate-active': indeterminate
   })
   const labelClass = classNames('ting-checkbox-label', {
@@ -24,19 +33,23 @@ const Checkbox:React.FC<CheckboxProps> = (props) => {
   })
   const handleChange= (e: React.ChangeEvent<HTMLInputElement>) => {
     onChange?.(e)
+    if(e.target.checked && selectOptions){
+      optionChange?.([...selectOptions, value])
+    }else {
+      optionChange?.(selectOptions!.filter(item => item !== value))
+    }
   }
   return (
     <label className={labelClass}>
       <span>
         <span className={spanClass}>
-          {indeterminate ? <Icon name="edit" /> :
-            <span className="change-span"/>}
+          {indeterminate? <span className="change-span"/> : <Icon name="select"/> }
         </span>
         <input
           ref={checkboxRef}
           type="checkbox"
           className="ting-checkbox-input"
-          checked={checked}
+          checked={_checked}
           onChange={handleChange}
           disabled={disabled}
         />
@@ -47,5 +60,6 @@ const Checkbox:React.FC<CheckboxProps> = (props) => {
     </label>
   )
 }
-
-export {Checkbox}
+Checkbox.displayName = "Checkbox"
+export {Checkbox};
+export type { CheckboxProps };
