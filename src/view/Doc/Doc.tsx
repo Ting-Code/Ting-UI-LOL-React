9020b271f2f416f79ad8e852e8db7eea6775b3b4
TINGCYGF <1127163160@qq.com>
2021-06-09 18:49:11 +0800

    修复点击top bug

--- src/view/Doc/Doc.tsx
+++ src/view/Doc/Doc.tsx
@@ -15,14 +15,14 @@ const Doc = () => {
     }
   })
   const onChangeAside = (e: React.MouseEvent) => {
-    const main = document.querySelector('.main-doc')
-    main!.scrollTop = 0
+
     const width = document.documentElement.clientWidth
     if(width <= 500){
       setAsideVisibleFn(false)
     }
     if(e.target instanceof HTMLAnchorElement){
-
+      const main = document.querySelector('.main-doc')
+      main!.scrollTop = 0
     }
   }
   return (
